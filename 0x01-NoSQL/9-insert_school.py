aa97ad8f8e44f25f31a5f51fecd8cefe28940e50
AturiheihiBlendon <2020blendon@gmail.com>
2023-02-13 23:36:46 +0300

    Task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Insert a document in Python
+"""
+
+def insert_school(mongo_collection, **kwargs):
+    """returns id of the inserted document
+    """
+    return mongo_collection.insert_one(kwargs)

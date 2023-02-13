11ffbc07da8e59e40ba39493e9ad42d0aae3d97a
AturiheihiBlendon <2020blendon@gmail.com>
2023-02-13 23:38:20 +0300

    Task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@ Insert a document in Python
 def insert_school(mongo_collection, **kwargs):
     """returns id of the inserted document
     """
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert(kwargs)

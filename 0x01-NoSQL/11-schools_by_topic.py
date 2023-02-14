f2c6f3f7253a5bec36cea702c3841d171f5a9f64
AturiheihiBlendon <2020blendon@gmail.com>
2023-02-14 10:16:24 +0300

    Task 11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+find a specific item in a collection
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns the list of school having a specific topic
+    """
+    return mongo_collection.find({"topic": topic})
